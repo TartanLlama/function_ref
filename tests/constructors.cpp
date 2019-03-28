01cc5c6dc483d6600ae1e95fb010dbc46eb19e27
Morten V. Pedersen <morten@mortenvp.com>
2019-03-28 11:18:45 +0100

    Silence warnings about unused variables in the tests

--- tests/constructors.cpp
+++ tests/constructors.cpp
@@ -1,13 +1,20 @@
 #include "catch.hpp"
 #include "function_ref.hpp"
 
-void foo(){}
-struct bar {
-    void baz(){}
+void foo() {}
+struct bar
+{
+    void baz() {}
 };
 
-TEST_CASE("Constructors", "[constructors]") {
-    tl::function_ref<void(void)> fr1 = []{};
+TEST_CASE("Constructors", "[constructors]")
+{
+    tl::function_ref<void(void)> fr1 = [] {};
     tl::function_ref<void(void)> fr2 = foo;
     tl::function_ref<void(bar)> fr3 = &bar::baz;
+
+    // Silence warnings
+    (void)fr1;
+    (void)fr2;
+    (void)fr3;
 }
