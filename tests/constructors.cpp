9232035ee2c1a25232d8dbf0274ec7874aca1014
Morten V. Pedersen <morten@mortenvp.com>
2019-03-28 11:32:35 +0100

    Undo formatting changes

--- tests/constructors.cpp
+++ tests/constructors.cpp
@@ -1,14 +1,12 @@
 #include "catch.hpp"
 #include "function_ref.hpp"
 
-void foo() {}
-struct bar
-{
-    void baz() {}
+void foo(){}
+struct bar {
+    void baz(){}
 };
 
-TEST_CASE("Constructors", "[constructors]")
-{
+TEST_CASE("Constructors", "[constructors]") {
     tl::function_ref<void(void)> fr1 = [] {};
     tl::function_ref<void(void)> fr2 = foo;
     tl::function_ref<void(bar)> fr3 = &bar::baz;
